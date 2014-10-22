e9d6fb89ca62b60fc14dde127c25e49c8dee29fc
danse <f.occhipinti@gmail.com>
2014-10-22 13:52:53 +0200

    add a file containing all locations

--- make.js
+++ make.js
@@ -3,15 +3,34 @@
 var fs = require('fs'),
     lib = require('./lib/make');
 
-[
+var modulesDir = './angular-modules/';
+
+console.log('fetching data');
+var countries = [
   'sierra_leone',
   'liberia'
-].forEach(function(country) {
+].map(function(country) {
   var data = require('./json/'+country+'.json');
-  var code = lib.modularise(country, data);
-  var fileName = 'angular-modules/'+country+'.js';
+  return ({
+    name: country,
+    data: data
+  });
+});
+
+console.log('generating modules containing a single country data each');
+countries.forEach(function(country) {
+  var code = lib.modularise(country);
+  var fileName = modulesDir+'loose/'+country.name+'.js';
   fs.writeFile(fileName, code, function(err) {
     if (err) throw err;
     console.log(fileName+' written');
   });
+});
+
+console.log('generating one module containing all country data');
+var code = lib.modulariseMultiple(countries);
+var fileName = modulesDir+'all.js';
+fs.writeFile(fileName, code, function(err) {
+  if (err) throw err;
+  console.log(fileName+' written');
 });
--- test/test.js
+++ test/test.js
@@ -1,8 +1,21 @@
 var lib = require('../lib/make'),
     assert = require('assert');
-describe('modularise', function() {
-  it('works', function() {
-    var code = lib.modularise('country', {region: 'Sicily'});
-    assert.equal(code, "angular.module('eHealth.locations', []).constant('locations_country', {\"region\":\"Sicily\"});");
+describe('the library', function() {
+  it('modularises a single country', function() {
+    var code = lib.modularise({
+      name: 'italy',
+      data: {region: 'Sicily'}
+    });
+    assert.equal(code, "angular.module('eHealth.locations', []).constant('locations_italy', {\"region\":\"Sicily\"});");
+  });
+  it('modularises multiple countries', function() {
+    var code = lib.modulariseMultiple([{
+      name: 'i',
+      data: {r: 's'}
+    }, {
+      name: 's',
+      data: {r: 'a'}
+    }]);
+    assert.equal(code, "angular.module('eHealth.locations', []).constant('locations_i', {\"r\":\"s\"}).constant('locations_s', {\"r\":\"a\"});");
   });
 });
