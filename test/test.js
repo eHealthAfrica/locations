930794c6c7ac303c74da514e5cbaca997c1f8c3c
emiguez <ernesto.miguez@asquera.de>
2015-08-25 15:01:47 +0200

    apply standardjs

--- test/test.js
+++ test/test.js
@@ -1,30 +1,31 @@
-var lib = require('../lib/make'),
-    assert = require('assert');
-describe('the library', function() {
-  it('modularises a single country', function() {
+/* global describe it */
+var lib = require('../lib/make')
+var assert = require('assert')
+describe('the library', function () {
+  it('modularises a single country', function () {
     var code = lib.modularise({
       name: 'italy',
       data: {region: 'Sicily'}
-    });
-    assert.equal(code, "angular.module('eHealth.locations', []).constant('locations_italy', {\"region\":\"Sicily\"});");
-  });
-  it('modularises a single country twice', function() {
+    })
+    assert.equal(code, 'angular.module(\'eHealth.locations\', []).constant(\'locations_italy\', {"region":"Sicily"});')
+  })
+  it('modularises a single country twice', function () {
     var country = {
       name: 'italy',
       data: {region: 'Sicily'}
-    };
-    var code = lib.modularise(country);
-    code = lib.modularise(country);
-    assert.equal(code, "angular.module('eHealth.locations', []).constant('locations_italy', {\"region\":\"Sicily\"});");
-  });
-  it('modularises multiple countries', function() {
+    }
+    var code = lib.modularise(country)
+    code = lib.modularise(country)
+    assert.equal(code, 'angular.module(\'eHealth.locations\', []).constant(\'locations_italy\', {"region":"Sicily"});')
+  })
+  it('modularises multiple countries', function () {
     var code = lib.modulariseMultiple([{
       name: 'i',
       data: {r: 's'}
     }, {
       name: 's',
       data: {r: 'a'}
-    }]);
-    assert.equal(code, "angular.module('eHealth.locations', []).constant('locations_i', {\"r\":\"s\"}).constant('locations_s', {\"r\":\"a\"});");
-  });
-});
+    }])
+    assert.equal(code, 'angular.module(\'eHealth.locations\', []).constant(\'locations_i\', {"r":"s"}).constant(\'locations_s\', {"r":"a"});')
+  })
+})
